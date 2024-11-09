3cac63a49c8d1cb4aa54ed92953d7d98c2178a1a
Ramrachai Marma <47687976+Ramrachai@users.noreply.github.com>
2024-11-09 11:20:07 +0600

    test commit to check webhook

--- express-app/app.js
+++ express-app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    console.log("test commit")
     res.send('Hello World from Express.js running on AWS Lambda!');
 });
 
